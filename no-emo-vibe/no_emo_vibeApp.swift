3dcb0da7cc6844554535963d3d84f4beb7d7ac2f
Yuhan Qiu <h237k77777@gmail.com>
2025-05-21 20:35:53 +0800

    Initial Commit

---
+++ no-emo-vibe/no_emo_vibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  no_emo_vibeApp.swift
+//  no-emo-vibe
+//
+//  Created by 邱宇涵 on 2025/5/21.
+//
+
+import SwiftUI
+
+@main
+struct no_emo_vibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
